9f33538264115303a08f0d3381ea4a4238bb5d6c
黄媚 <hm5yes@163.com>
2020-07-10 10:37:13 +0800

    commit B

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!AAA"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Hello World!BB');
     });
   });
 });
